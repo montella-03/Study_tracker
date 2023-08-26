310442b667b7d2422efe20cb743a19bd929254a6
CHRISTOPHER OIGO <oigoc60@gmail.com>
2023-08-26 23:35:16 +0300

    dev1

--- frontend/views/OrdersView.tsx
+++ frontend/views/OrdersView.tsx
@@ -1,11 +1,21 @@
 import MainLayout from "Frontend/views/Layouts/MainLayout";
 import Navbar from "Frontend/components/Navbar";
+import React from "react";
+import {Notification} from "@hilla/react-components/Notification";
 
 const topics = ["All", "Pending", "Completed", "Cancelled"];
 const OrdersView = () => {
+    const handleClick=()=> {
+        return(
+        <Notification>
+            <p>Clicked</p>
+        </Notification>
+        )
+    }
+
     return (
         <MainLayout>
-            <Navbar title={"Orders"} topics={topics}/>
+            <Navbar title={"Orders"} topics={topics} handleClick={()=>{}}/>
         </MainLayout>
     )
 }
